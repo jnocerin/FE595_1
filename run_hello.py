0fe58aab34f689a1d23e6c409a934a1dca950167
jnocerin <jnocerin@gmail.com>
2019-10-13 14:38:20 -0700

    Update run_hello.py

--- run_hello.py
+++ run_hello.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-#@app.route('/hello', methods=['GET', 'POST'])
-#def hello_world():
-#    print("What should I print?")
-#    return "Hello, World!!!"
+@app.route('/hello', methods=['GET', 'POST'])
+def hello_world():
+    print("What should I print?")
+    return "Hello, World!!!"
     
 @app.route('/hello/<name>', methods=['GET'])
 def hello_person(name):
