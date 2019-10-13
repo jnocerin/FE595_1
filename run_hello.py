74c21849c114aec08b16c964ebbbfdcae4e70bdd
jnocerin <jnocerin@gmail.com>
2019-10-13 12:31:45 -0700

    Update run_hello.py

--- run_hello.py
+++ run_hello.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/hello', methods=['GET', 'POST'])
@@ -8,7 +9,8 @@ def hello_world():
     
 @app.route('/hello/<name>', methods=['GET'])
 def hello_person(name):
-    return "Hello,  {}!".format(name)
+     return render_template(name)
+#    return "Hello,  {}!".format(name)
   
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
